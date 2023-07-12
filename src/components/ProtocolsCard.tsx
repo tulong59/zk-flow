d8519be042c1f2051daf9d18dff920820c9ced66
Théo Maringer <theo.maringer@epitech.eu>
2023-07-12 11:10:02 +0200

    feat: xyfinance and add new address for holdstation

--- src/components/ProtocolsCard.tsx
+++ src/components/ProtocolsCard.tsx
@@ -19,6 +19,7 @@ import { Goal3 } from '../protocols/goal3.ts';
 import { Rollup } from '../protocols/rollup.ts';
 import { ZkSyncId } from '../protocols/zksyncid.ts';
 import { ZkSwap } from '../protocols/zkswap.ts';
+import { XYFinance } from '../protocols/xyfinance.ts';
 
 interface ProtocolsCardProps {
   address: string;
@@ -59,6 +60,7 @@ const ProtocolsCard: FC<ProtocolsCardProps> = ({ address, transactions }) => {
     setProtocolsState((prevState) => [...prevState, Rollup.getProtocolsState(transactions, address)]);
     setProtocolsState((prevState) => [...prevState, ZkSyncId.getProtocolsState(transactions, address)]);
     setProtocolsState((prevState) => [...prevState, ZkSwap.getProtocolsState(transactions, address)]);
+    setProtocolsState((prevState) => [...prevState, XYFinance.getProtocolsState(transactions, address)]);
 
     setProtocolsState((prevState) => prevState.sort((a, b) => b.volume - a.volume));
   };
--- src/protocols/xyfinance.ts
+++ src/protocols/xyfinance.ts
@@ -2,27 +2,37 @@ import { Transaction } from '../services/explorer.ts';
 import { ProtocolState } from '../components/ProtocolsCard.tsx';
 import { countTransactionPeriods } from '../utils/utils.ts';
 
-const holdstationAddresses: string[] = [
-  '0x7b4872e2096ec9410b6b8c8b7d039589e6ee8022',
-  '0xaf08a9d918f16332f22cf8dc9abe9d9e14ddcbc2',
-  '0x51956481ced6f65458a25207e725cbd2e33a02cb',
+const xyfinanceAddresses: string[] = [
+  '0x936fef4245f281ed4f2ee303060a8656399dcc32',
+  '0xada6a5c2ddf499b50efced4c489a90b707693ac5',
+  '0xaa0b0654e79e17332d983e2351bd926ce336b9bd',
+  '0x935283a00fbf8e40fd2f8c432a488f6addc8db67',
+  '0xdf4700daccbfb097204576cbbb4728d4f190fdc1',
+  '0x75167284361c8d61be7e4402f4953e2b112233cb',
+  '0xc81dc052661785c5a304ebb2c4bceadcfb79a675',
+  '0x9e52c2bd0177404dca3e122992a7fc0bcb15fd95',
+  '0x83529b846762d1146c251548a3d8e453c1d09424',
+  '0xbcdde9ca12f1141943e98249dae5b18a3d885e6b',
+  '0x48ca44863861fd0687b3a21211bf90cb5924704e',
+  '0xe4e156167cc9c7ac4abd8d39d203a5495f775547',
+  '0x30e63157bd0ba74c814b786f6ea2ed9549507b46',
 ];
 
-export const Holdstation = {
+export const XYFinance = {
   getProtocolsState: (transactions: Transaction[], address: string) => {
     const protocolState: ProtocolState = {
-      name: 'Holdstation',
-      id: 'holdstation',
+      name: 'XY Finance',
+      id: 'xyfinance',
       lastActivity: '',
       volume: 0,
       interactions: 0,
       activeDays: 0,
       approves: 0,
-      url: 'https://holdstation.exchange/',
+      url: 'https://xy.finance/',
     };
 
     transactions.forEach((transaction: Transaction) => {
-      if (holdstationAddresses.includes(transaction.to.toLowerCase())) {
+      if (xyfinanceAddresses.includes(transaction.to.toLowerCase())) {
         if (protocolState.lastActivity === '') protocolState.lastActivity = transaction.receivedAt;
         if (new Date(protocolState.lastActivity) < new Date(transaction.receivedAt))
           protocolState.lastActivity = transaction.receivedAt;
@@ -44,7 +54,7 @@ export const Holdstation = {
       address,
       transactions,
       protocolState.id,
-      holdstationAddresses,
+      xyfinanceAddresses,
     ).days;
 
     return protocolState;
