253f3b692f4e2b451315aadb57f2f47a5b54deb9
Théo Maringer <theo.maringer@epitech.eu>
2023-08-05 20:55:17 +0200

    feat: fix izumi finance

--- src/protocols/izumi.ts
+++ src/protocols/izumi.ts
@@ -3,18 +3,9 @@ import { ProtocolState } from '../components/ProtocolsCard.tsx';
 import { countTransactionPeriods } from '../utils/utils.ts';
 
 const izumiFinanceAddresses = [
-  '0x33d9936b7b7bc155493446b5e6ddc0350eb83aec',
-  '0x7499ce9c8f4ff47be5dd5308ab54cc710de751e1',
-  '0xbc94aedd2a0a986476b89e072b05e0df117a3f8b',
-  '0xc319755dff1601b3d0520b421a281b11bf22e80f',
-  '0x8df80089b7ab1646db211d43949ecdfc94582011',
-  '0x0066f3791bd9d5a25d88f978dd8e1006445fe0d6',
-  '0x377ec7c9ae5a0787f384668788a1654249059dd6',
-  '0x3ec82c07981d6d213da9bd35a0ba4cd324fea438',
-  '0x9606ec131eec0f84c95d82c9a63959f2331cf2ac',
-  '0x936c9a1b8f88bfdbd5066ad08e5d773bc82eb15f',
-  '0x8b9d7d609a83b2f69d2135786a7d230043af7283',
-  '0x7dee7de9814ed6c1e20b3e4e2fa9b1b96e15fde1',
+  '0x943ac2310d9bc703d6ab5e5e76876e212100f894',
+  '0x83109541318bbd104c9c85328696107f5767dffb',
+  '0x483fde31bce3dcc168e23a870831b50ce2ccd1f1',
 ];
 
 export const IzumiFinance = {
